88781dd1560541aca184aa7d79a2fd58e885eb97
David7ce <david7ce@outlook.com>
2023-04-23 04:20:55 +0100

    fix arksys-repo symlink and created =x86_64

--- update_repo.sh
+++ update_repo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#touch ./arksys-repo.db.tar.gz
+touch -c ./x86_64/arksys_repo.db.tar.gz
 
 # add all packages with ".pkg.tar.zst" extension in that path to the database
-repo-add ./arksys-repo.db.tar.gz ./*.pkg.tar.zst
+repo-add ./x86_64/arksys_repo.db.tar.gz ./x86_64/*.pkg.tar.zst
 
 echo "####################################"
 echo "    ArkSys x86 Repo Updated!!    "
 echo "####################################"
-
-
