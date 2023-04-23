aae300fb78981c0422368af38cdfcf83ad9f7ffb
David7ce <david7ce@outlook.com>
2023-04-23 15:45:32 +0100

    add again symlink to download with proper format

--- update_repo.sh
+++ update_repo.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-touch -c ./x86_64/arksys_repo.db.tar.gz
+cd ./x86_64/
+
+touch -c arksys-repo.db.tar.gz
 
 # add all packages with ".pkg.tar.zst" extension in that path to the database
-repo-add ./x86_64/arksys_repo.db.tar.gz ./x86_64/*.pkg.tar.zst
+repo-add arksys-repo.db.tar.gz *.pkg.tar.zst
+
+echo "Arksys packages added"
+echo "Romve symlinks and rename database"
+
+rm arksys-repo.db
+rm arksys-repo.files
+
+mv arksys-repo.db.tar.gz arksys-repo.db
+mv arksys-repo.files.tar.gz arksys-repo.files
 
 echo "####################################"
-echo "    ArkSys x86 Repo Updated!!    "
+echo "      ArkSys x86 Repo Updated!!     "
 echo "####################################"
