6bb8a1c3582d8a2524bca4eb2eb58bcb7d6edf86
David7e <david7ce@outlook.com>
2023-08-22 21:02:57 +0100

    database to tar.gz

--- update_repo.sh
+++ update_repo.sh
@@ -3,19 +3,20 @@
 cd ./x86_64/
 
 # add all packages with ".pkg.tar.zst" extension in that path to the database
-repo-add arksys-repo.db.tar.zst *.pkg.tar.zst
+# the database is a tarball with extensions: .tar.gz, .tar.bz2, .tar.xz, .tar.zst, or .tar.Z.
+repo-add arksys-repo.db.tar.gz *.pkg.tar.zst
 
 # remove symlinks for local repository
-rm arksys-repo.db
-rm arksys-repo.files
+#rm arksys-repo.db
+#rm arksys-repo.files
 
 # rename files without ".tar.gz" because the symlinks do not exist
-mv arksys-repo.db.tar.gz arksys-repo.db
-mv arksys-repo.files.tar.gz arksys-repo.files
+#mv arksys-repo.db.tar.gz arksys-repo.db
+#mv arksys-repo.files.tar.gz arksys-repo.files
 
 # remove old files
-rm arksys-repo.db.tar.zst.old
-rm arksys-repo.files.tar.zst.old
+rm arksys-repo.db.tar.*.old
+rm arksys-repo.files.tar.*.old
 
 echo "####################################"
 echo "      ArkSys x86 Repo Updated!!     "
